bfa14e06cf21e3fd34f8d42cf3513602644bf51c
Rodrigo Marques <164966269+RodrigoLorensiMarques@users.noreply.github.com>
2026-05-10 21:39:46 -0300

    feat: adiciona função de recuperação de usuários da db

--- SessionChecker/app/services/surf_service.py
+++ SessionChecker/app/services/surf_service.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.local_monitoring import LocalMonitoring
+from app.models.user import User
+
 
 
 
@@ -7,3 +9,5 @@ def get_locals(db:Session):
     return db.query(LocalMonitoring).all()
 
 
+def get_users(db:Session):
+    return db.query(User).all()
--- SessionChecker/workers/check_sessions.py
+++ SessionChecker/workers/check_sessions.py
@@ -1,19 +1,25 @@
 from app.services.surf_service import get_locals
+from app.services.surf_service import get_users
+
 from app.db.session import SessionLocal
 from app.scrapers.main import screper
 
 db = SessionLocal()
 
 local_list = get_locals(db)
+adress_list = get_users(db)
 
 
 
-def check_locals(local_list):
+def check_locals(local_list, adress_list):
     for local in local_list:
         local_check = screper(local.name_local)
 
         if local_check:
-            print ("Has session")
-        
+            for adress in adress_list:
+                if adress.local_monitoring_id  == local.id:
+                    #send email
 
-check_locals(local_list)
+                    print()
+
+check_locals(local_list, adress_list)
